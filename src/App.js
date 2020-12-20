106eeb8a66375291716a2f49c0e400f6d969a958
Stefan Natter <stefan@natter.at>
2020-12-20 16:23:08 +0100

    feat: example from youtube live stream
    see https://www.youtube.com/watch?v=Nb13Ff7v30k

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import logo from './logo.svg'
 import './App.css'
 
-function App() {
+function App({ backgroundColor }) {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header" style={{ backgroundColor }}>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React with the backgroundColor {backgroundColor}
         </a>
       </header>
     </div>
--- src/App.stories.js
+++ src/App.stories.js
@@ -1,3 +1,13 @@
+/**
+ * Example Story from the YouTube Live Stream
+ * @see https://www.youtube.com/watch?v=Nb13Ff7v30k
+ *
+ * References
+ * @see https://www.chromatic.com/pricing
+ * @see https://storybook.js.org/addons
+ * @see https://www.npmjs.com/package/@storybook/addon-a11y
+ * @see http://localhost:6006/?path=/story/create-react-app-demo-app--dark-mode-app
+ */
 import React from 'react'
 
 import App from './App'
@@ -5,6 +15,31 @@ import App from './App'
 export default {
   title: 'Create React App/App',
   component: App,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
 }
 
-export const app = () => <App />
+export const darkModeApp = args => <App {...args} />
+
+darkModeApp.args = {
+  backgroundColor: 'transparent',
+}
+
+darkModeApp.parameters = {
+  backgrounds: {
+    default: 'dark',
+  },
+}
+
+export const lightModeApp = args => <App {...args} />
+
+lightModeApp.args = {
+  backgroundColor: 'transparent',
+}
+
+lightModeApp.parameters = {
+  backgrounds: {
+    default: 'light',
+  },
+}
